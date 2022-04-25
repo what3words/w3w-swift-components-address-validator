b3b6924fc5069a863c3cb971705371b72304138b
Dave Duprey <dave@what3words.com>
2022-04-25 13:57:45 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# w3w-swift-components-street-addresses
+
+A description of this package.
---
+++ Sources/w3w-swift-components-street-addresses/w3w_swift_components_street_addresses.swift
@@ -0,0 +1,6 @@
+public struct w3w_swift_components_street_addresses {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
